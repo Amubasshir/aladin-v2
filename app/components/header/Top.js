cc7b134699ca25857c710164f3381deee065ccd4
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-07-02 15:37:08 +0600

    topjs done for header

--- app/components/header/Top.js
+++ app/components/header/Top.js
@@ -1,5 +1,73 @@
-const Top = () => {
-  return <div>Top</div>;
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { useState } from 'react';
+import { BsSuitHeart } from 'react-icons/bs';
+import { CiMobile1 } from 'react-icons/ci';
+import { MdSecurity } from 'react-icons/md';
+import { RiAccountPinCircleLine, RiArrowDropDownFill } from 'react-icons/ri';
+import UserMenu from './UserMenu';
+import styles from './styles.module.scss';
+const Top = ({ country }) => {
+  const { data: session } = useSession();
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className={styles.top}>
+      <div className={styles.top_container}>
+        <div></div>
+        <ul className={styles.top_list}>
+          <li className={styles.li}>
+            <CiMobile1 />
+            <span>App</span>
+          </li>
+          <li className={styles.li}>
+            <img src="" alt="" />
+            <span>{country.name} /tk</span>
+          </li>
+          <li className={styles.li}>
+            <MdSecurity />
+            <span>Buyer Protection</span>
+          </li>
+
+          <li className={styles.li}>
+            <span>Customer Service</span>
+          </li>
+          <li className={styles.li}>
+            <span>Help</span>
+          </li>
+          <li className={styles.li}>
+            <BsSuitHeart />
+            <Link href="/profile/wishlist">
+              <span>Wishlist</span>
+            </Link>
+          </li>
+          <li
+            className={styles.li}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
+            {session ? (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <img src={session.user.image} alt="" />
+                  <span>{session.user.name}</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            {visible && <UserMenu session={session} />}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
