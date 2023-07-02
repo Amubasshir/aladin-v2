490ce3d4dbce92de52b652f0e22f6329b1568af0
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-07-02 15:35:28 +0600

    mainjs done for header

--- app/components/header/Main.js
+++ app/components/header/Main.js
@@ -1,5 +1,34 @@
+import Link from 'next/link';
+import { FaOpencart } from 'react-icons/fa';
+
+import { RiSearch2Line } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
+import styles from './styles.module.scss';
 const Main = () => {
-  return <div>Main</div>;
+  const { cart } = useSelector((state) => ({ ...state }));
+  return (
+    <div className={styles.main}>
+      <div className={styles.main_container}>
+        <Link legacyBehavior href="/">
+          <a className={styles.logo}>
+            <img src="https://i.ibb.co/qdcJDqJ/aladin-text-logoo.png" alt="" />
+          </a>
+        </Link>
+        <div className={styles.search}>
+          <input type="text" placeholder="Search" />
+          <div className={styles.search_icon}>
+            <RiSearch2Line />
+          </div>
+        </div>
+        <Link legacyBehavior href="/cart">
+          <a className={styles.cart}>
+            <FaOpencart />
+            <span>0</span>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
